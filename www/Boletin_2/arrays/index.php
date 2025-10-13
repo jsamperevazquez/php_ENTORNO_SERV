629f18c88d4524fa2d606a212b5373b4937b66a6
JSampereVázquez <72076376+jsamperevazquez@users.noreply.github.com>
2025-10-13 12:49:05 +0200

    Foreach in arrays

--- www/Boletin_2/arrays/index.php
+++ www/Boletin_2/arrays/index.php
@@ -6,4 +6,13 @@ for ($i = 0; $i <= 10; $i++) {
     }
 }
 print_r($numeros_pares);
+
+$v[1] = 90;
+$v[10] = 200;
+$v['hola'] = 43;
+$v[9] = 'e';
+
+foreach ($v as $key => $value) {
+    echo $key . ' -> ' . $value . PHP_EOL;
+}
 ?>
