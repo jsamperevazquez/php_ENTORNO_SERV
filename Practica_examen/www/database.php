7c4bf311e7a3e9cc8716c139d7ae1b39208d281d
JSampereVázquez <72076376+jsamperevazquez@users.noreply.github.com>
2025-12-04 13:46:04 +0100

    Bug fixed

--- Practica_examen/www/database.php
+++ Practica_examen/www/database.php
@@ -36,9 +36,11 @@ function conectaPDO()
     ];
     try {
         $connPDO = new PDO("mysql:host={$mysqlData['host']};dbname={$mysqlData['dbname']}", $mysqlData['user'], $mysqlData['pass']);
-        $connPDO = new PDO("mysql:host={$mysqlData['host']};dbname={$mysqlData['dbname']}", $mysqlData['user'], $mysqlData['pass']);
         $connPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $connPDO;
+        if ($connPDO->connect_error) {
+            throw new Exception("Error de conexión: " . $connPDO->connect_error);
+        }
     } catch (PDOException $e) {
         echo "<div class='alert alert-danger' role='alert'>" . $e->getMessage() . "</div>";
         return null;
